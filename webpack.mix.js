8f382fecbb268dda94db9fb1712cea6c8b468469
Mehul Bawadia <mehulbawadia@gmail.com>
2020-06-08 16:39:11 +0530

    Added selectize and iGrowl jquery plugin

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,12 @@ let glob           = require("glob-all");
 let PurgecssPlugin = require("purgecss-webpack-plugin");
 require('laravel-mix-purgecss');
 
+class TailwindExtractor {
+    static extract(content) {
+        return content.match(/[A-z0-9-:\/]+/g);
+    }
+}
+
 mix.sass('resources/sass/app.scss', 'public/css')
    .options({
         processCssUrls: false,
@@ -15,4 +21,17 @@ mix.styles(['public/css/app.css'], 'public/css/app.css').purgeCss({
     enabled: mix.inProduction()
 });
 
-mix.js(['resources/js/app.js'], 'public/js/app.js');
+mix.js([
+    'resources/js/app.js',
+    'resources/libs/iGrowl/javascripts/igrowl.js',
+    'resources/libs/selectizeJs/js/standalone/selectize.min.js',
+], 'public/js/app.js');
+
+mix.styles([
+    'resources/libs/iGrowl/stylesheets/animate.css',
+    'resources/libs/iGrowl/stylesheets/igrowl.css',
+    'resources/libs/iGrowl/stylesheets/icomoon/feather.css',
+    'resources/libs/selectizeJs/css/selectize.css',
+], 'public/css/vendor.css');
+
+mix.copyDirectory('resources/libs/iGrowl/fonts/feather', 'public/fonts/feather');
