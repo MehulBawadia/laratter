671ed23eb22f9ca7722fb93280e5e759f413a7fc
Mehul Bawadia <mehulbawadia@gmail.com>
2020-06-17 15:59:44 +0530

    Changed dashboard header

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,13 +1,13 @@
 @extends('admin.partials._layout')
 
 @section('title')
-    <title>Login Administrator | {{ config('app.name') }}</title>
+    <title>Welcome {{ auth()->user()->username }} | {{ config('app.name') }}</title>
 @endsection
 
 @section('content')
-    <section class="px-3 my-5">
+    <section class="px-3 py-5 bg-white shadow">
         <div class="container mx-auto">
-            <h1>Welcome {{ auth()->user()->username }}</h1>
+            <h1 class="text-2xl font-bold">Dashboard</h1>
         </div>
     </section>
 @endsection
