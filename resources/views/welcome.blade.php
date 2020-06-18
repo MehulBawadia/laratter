8a4eceb7c5dc27850972bdc8040379c5d2527b65
Mehul Bawadia <mehulbawadia@gmail.com>
2020-06-18 17:00:54 +0530

    Made desgin tweaks on home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,22 @@
-Welcome Home Page
+@extends('partials._layout')
+
+@section('title')
+    <title>{{ config('app.name') }}</title>
+@endsection
+
+@section('content')
+    <div class="container mx-auto">
+        <div class="h-screen flex items-center justify-center">
+            <div class="px-5 py-5">
+                <h1 class="text-center uppercase font-bold tracking-widest text-gray-600 text-2xl sm:text-3xl lg:text-5xl">
+                    {{ config('app.name') }}
+                </h1>
+
+                <div class="flex flex-wrap justify-evenly mt-5">
+                    <a href="{{ route('user.login') }}" class="text-gray-600 font-bold tracking-widest">Login</a>
+                    <a href="{{ route('user.register') }}" class="text-gray-600 font-bold tracking-widest">Register</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
